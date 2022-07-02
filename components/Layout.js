133e3b9341d80df1c9e5a1bcb2846de3b03986c9
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-02 19:21:22 +0300

    add home and layout

---
+++ components/Layout.js
@@ -0,0 +1,5 @@
+const Layout = ({ children }) => {
+  return <main className="md:px-[10vw] py-8 bg-[#eef0f1]">{children}</main>;
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className="text-3xl underline text-red-500">Hello World!</h1>
-    </div>
+    <Layout>
+      <h1 className="font-bold uppercase text-3xl text-center mb-12">Feed</h1>
+      <div className="border border-[#b5bdc4] p-8 rounded bg-white">
+        <a href="#" className="font-bold">
+          {" "}
+          By @bobbysue{" "}
+        </a>
+        <h2 className="my-4 font-bold text-xl">I Like Turtles</h2>
+        <div className="flex justify-between">
+          <span>Comments: 5</span>
+          <span> 💗 0 Hearts </span>
+        </div>
+      </div>
+    </Layout>
   );
 }
