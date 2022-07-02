8ee874910a99c5e65c223cc805b7cf8d7736f0a7
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-02 19:21:42 +0300

    Create Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,76 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white py-2">
+      <ul className="px-[10vw] flex justify-between items-center">
+        <div className="flex items-center gap-4">
+          <li>
+            <Link href="/">
+              <button className="px-4 py-2 bg-black text-white rounded text-2xl font-bold hover:brightness-90">
+                BLOG
+              </button>
+            </Link>
+          </li>
+
+          <li className="hidden md:block">
+            <form method="get" className="relative">
+              <input
+                type="text"
+                placeholder="Search..."
+                name="search"
+                className="border border-[#dfdfdf] rounded px-4 py-2 lg:pr-20 focus:outline-[#3b49df]"
+              />
+            </form>
+          </li>
+        </div>
+
+        {/* <!-- Guest Nav --> */}
+        <div className="flex gap-2 items-center">
+          <li className="hidden md:block">
+            <a
+              href="#"
+              className="text-[#626262] text-lg hover:bg-[#EBECFC] px-4 py-2 rounded hover:text-[#3b49df] hover:underline"
+            >
+              Log in
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="#"
+              className="text-[#3b49df] font-semibold border border-[#3b49df] rounded px-4 py-3 hover:underline hover:text-white hover:bg-[#3b49df]"
+            >
+              Create account
+            </a>
+          </li>
+        </div>
+
+        {/* <!-- User Nav --> */}
+        {/* <div className="flex gap-2 items-center">
+          <li>
+            <button
+              className="px-4 py-2 rounded text-[#7f8893] hover:underline hover:bg-[#e5e5e5] hover:text-[#3b49df]"
+            >
+              Sign out
+            </button>
+          </li>
+
+          <li>
+            <a
+              href="#"
+              className="text-[#3b49df] font-semibold border border-[#3b49df] rounded px-4 py-3 hover:underline hover:text-white hover:bg-[#3b49df]"
+              >Write Post</a
+            >
+          </li>
+
+          <li className="w-[60px]">
+            <img src="/img/hacker.jpg" alt="profile pic" />
+          </li>
+        </div> */}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
