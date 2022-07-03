6e60ad0543a56d33c9b8fbe7d0f0866d5af8aea8
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-03 16:42:57 +0300

    create auth context

---
+++ contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const setUserData = (userData) => setUser(userData);
+
+  const removeUserData = () => setUserData(null);
+
+  return (
+    <AuthContext.Provider value={{ user, setUserData, removeUserData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  const state = useContext(AuthContext);
+
+  return state;
+};
