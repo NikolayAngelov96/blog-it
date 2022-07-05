0cd84c924b013eedd74c6c7fb2f4af057f8a3b6c
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-06 00:31:43 +0300

    fix posts to be in desc order

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -13,7 +13,9 @@ export default async function handler(req, res) {
 
       await dbConnect();
 
-      const posts = await Post.find({ owner: payload._id }).populate("owner");
+      const posts = await Post.find({ owner: payload._id })
+        .populate("owner")
+        .sort({ createdAt: -1 });
 
       res.status(200).json({ posts });
     } catch (err) {
