139839d7832690c8a4999e975751b95b413882be
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-05 11:30:28 +0300

    add post create api endpoint

---
+++ pages/api/post/create.js
@@ -0,0 +1,33 @@
+import * as jwt from "jsonwebtoken";
+import dbConnect from "../../../lib/dbConnect";
+import Post from "../../../models/Post";
+import User from "../../../models/User";
+
+const SECRET = process.env.JWT_SECRET;
+
+export default async function handler(req, res) {
+  if (req.method == "POST") {
+    const token = req.headers["x-authorization"];
+
+    try {
+      const payload = jwt.verify(token, SECRET);
+
+      const data = JSON.parse(req.body);
+
+      await dbConnect();
+
+      const user = await User.findById(payload._id);
+
+      const post = await Post.create({ ...data, owner: user._id });
+
+      user.posts.push(post);
+
+      await user.save();
+
+      res.status(201).json({ post });
+    } catch (err) {
+      console.error(err);
+      res.status(400).json({ message: err.message });
+    }
+  }
+}
