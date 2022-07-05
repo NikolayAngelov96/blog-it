eaa56fd51687d4fde31c4528708fedf45e11f27e
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-06 00:32:02 +0300

    add post update api endpoint

--- next.config.js
+++ next.config.js
@@ -25,6 +25,15 @@ module.exports = {
           },
         ],
       },
+      {
+        source: "/post/update",
+        headers: [
+          {
+            key: "X-Authorization",
+            value: "",
+          },
+        ],
+      },
     ];
   },
 };
---
+++ pages/api/post/update.js
@@ -0,0 +1,26 @@
+import * as jwt from "jsonwebtoken";
+import dbConnect from "../../../lib/dbConnect";
+import Post from "../../../models/Post";
+
+const SECRET = process.env.JWT_SECRET;
+
+export default async function handler(req, res) {
+  if (req.method == "POST") {
+    const token = req.headers["x-authorization"];
+    try {
+      const payload = jwt.verify(token, SECRET);
+
+      const data = JSON.parse(req.body);
+
+      const { postId, content, published } = data;
+
+      await dbConnect();
+
+      await Post.findByIdAndUpdate(postId, { content, published });
+
+      res.status(200).json({ message: "Successfully updated" });
+    } catch (err) {}
+  } else {
+    res.status(400).json({ message: "Invalid request type" });
+  }
+}
