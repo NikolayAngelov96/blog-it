4f0296c2bb2f45f8d03a0fcc65857e12044c801d
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-07 01:09:33 +0300

    add reqest to endpoint for like/dislike

--- components/HeartButton.js
+++ components/HeartButton.js
@@ -1,16 +1,74 @@
-const HeartButton = ({ isLiked }) => {
+import toast from "react-hot-toast";
+import { useAuthContext } from "../contexts/AuthContext";
+
+// Heart and UnHeart not reacting to like change
+
+const HeartButton = ({ post, incrementHeartCount, decrementHeartCount }) => {
+  const { user } = useAuthContext();
+
+  let isLiked = post.hearts.some((x) => x == user._id);
+
   const addHeart = async () => {
     //TODO: add heart with userId to collection
+
+    try {
+      const res = await fetch("http://localhost:3000/api/post/like", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ postId: post._id, userId: user._id }),
+      });
+
+      if (res.ok != true) {
+        let error = await res.json();
+        throw new Error(error.message);
+      }
+
+      incrementHeartCount();
+      isLiked = true;
+    } catch (err) {
+      toast.error(err.message);
+    }
   };
 
+  console.log(decrementHeartCount);
   const removeHeart = async () => {
     //TODO: remove heart from collection
+    try {
+      const res = await fetch("http://localhost:3000/api/post/unlike", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ postId: post._id, userId: user._id }),
+      });
+
+      if (res.ok != true) {
+        let error = await res.json();
+        throw new Error(error.message);
+      }
+
+      decrementHeartCount();
+      isLiked = false;
+    } catch (err) {
+      console.error(err);
+      toast.error(err.message);
+    }
   };
 
   return isLiked ? (
-    <button className="px-4 py-2 bg-red-500">💔 Unheart</button>
+    <button
+      className="py-3 bg-[#b5bdc4] text-black rounded w-full"
+      onClick={removeHeart}
+    >
+      💔 Unheart
+    </button>
   ) : (
-    <button className="py-3  bg-[#b5bdc4] text-black rounded w-full">
+    <button
+      className="py-3 bg-[#b5bdc4] text-black rounded w-full"
+      onClick={addHeart}
+    >
       💗 Heart
     </button>
   );
