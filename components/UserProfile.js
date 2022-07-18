1dd03d98fd4bc536d7cf02f12d7b0e30ec29715b
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2022-07-18 16:59:44 +0300

    add avatar

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -3,13 +3,9 @@ import Image from "next/image";
 const UserProfile = ({ user }) => {
   return (
     <div className="text-center">
-      <Image
-        src={user?.avatar || `/img/hacker.jpg`}
-        width={144}
-        height={144}
-        alt="Profile Picture"
-        className="rounded-full"
-      />
+      <div className="w-[144px] h-[144px] mx-auto">
+        <img src={user?.avatar || "/img/hacker.jpg"} className="rounded-full" />
+      </div>
       <p>{user.email}</p>
       <h1 className="text-3xl font-bold my-4 capitalize">{user.username}</h1>
     </div>
